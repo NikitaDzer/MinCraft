b3393896e909d4df6e8a5e7decf82007ba0a5fc5
Sergei Zimmerman <serjtsimmerman@gmail.com>
2023-02-22 10:24:49 +0300

    Configure clang-format

--- utils/vkinfo.cc
+++ utils/vkinfo.cc
@@ -4,62 +4,73 @@
 
 #include <fmt/core.h>
 
-#include "unified_includes/vulkan_include.h"
-
-#include <string>
-#include <numeric>
 #include <algorithm>
-#include <sstream>
 #include <cstdint>
+#include <numeric>
+#include <sstream>
+#include <string>
+
+#include "unified_includes/vulkan_include.h"
 
 namespace
 {
 
 constexpr auto k_application_info = vk::ApplicationInfo{
-    .pApplicationName = "vkinfo", .applicationVersion = VK_MAKE_VERSION( 0, 0, 1 ),
-    .pEngineName = "no engine", .engineVersion = VK_MAKE_VERSION( 0, 0, 1 ), .apiVersion = VK_VERSION_1_1
-};
+    .pApplicationName = "vkinfo",
+    .applicationVersion = VK_MAKE_VERSION( 0, 0, 1 ),
+    .pEngineName = "no engine",
+    .engineVersion = VK_MAKE_VERSION( 0, 0, 1 ),
+    .apiVersion = VK_VERSION_1_1 };
 
-std::string versionToString(uint32_t version) {
-    const auto ver_major = VK_VERSION_MAJOR(version);
-    const auto ver_minor = VK_VERSION_MINOR(version);
-    const auto ver_patch = VK_VERSION_PATCH(version);
-    return fmt::format("{}.{}.{}", ver_major, ver_minor, ver_patch);
+std::string
+versionToString ( uint32_t version )
+{
+    const auto ver_major = VK_VERSION_MAJOR( version );
+    const auto ver_minor = VK_VERSION_MINOR( version );
+    const auto ver_patch = VK_VERSION_PATCH( version );
+    return fmt::format( "{}.{}.{}", ver_major, ver_minor, ver_patch );
 }
 
 void
-printPhysicalDeviceProperties( vk::PhysicalDevice device )
+printPhysicalDeviceProperties ( vk::PhysicalDevice device )
 {
     const auto extensions = device.enumerateDeviceExtensionProperties();
     const auto properties = device.getProperties();
 
-    fmt::println( "Found physical device: [id = {}, type = {}, name = {}, version = {}]",
-                  properties.deviceID, vk::to_string( properties.deviceType ), properties.deviceName, versionToString(properties.apiVersion) );
+    fmt::println(
+        "Found physical device: [id = {}, type = {}, name = {}, version = {}]",
+        properties.deviceID,
+        vk::to_string( properties.deviceType ),
+        properties.deviceName,
+        versionToString( properties.apiVersion )
+    );
 
-    const auto extensions_string = std::accumulate( extensions.begin(), extensions.end(), std::string{},
-                                                    [] (auto str, auto ext) {
+    const auto extensions_string =
+        std::accumulate( extensions.begin(), extensions.end(), std::string{}, [] ( auto str, auto ext ) {
             return str + " " + ext.extensionName.data();
         } );
 
-    fmt::println("Device [{}] supports following extensions: {}", properties.deviceName, extensions_string);
+    fmt::println( "Device [{}] supports following extensions: {}", properties.deviceName, extensions_string );
 }
 
-}
+} // namespace
 
 int
-main () try
+main ()
+try
 {
-    const auto instance_create_info = vk::InstanceCreateInfo{.pApplicationInfo = &k_application_info};
+    const auto instance_create_info = vk::InstanceCreateInfo{ .pApplicationInfo = &k_application_info };
     auto instance = vk::createInstanceUnique( instance_create_info );
 
     const auto physical_devices = instance->enumeratePhysicalDevices();
-    for (const auto & device: physical_devices)
+    for ( const auto& device : physical_devices )
     {
         printPhysicalDeviceProperties( device );
     }
-
-} catch (vk::Error &e) {
-    fmt::println("Vulkan error: {}", e.what());
-} catch (std::exception &e) {
-    fmt::println("Error: {}", e.what());
+} catch ( vk::Error& e )
+{
+    fmt::println( "Vulkan error: {}", e.what() );
+} catch ( std::exception& e )
+{
+    fmt::println( "Error: {}", e.what() );
 }
