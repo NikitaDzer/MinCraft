fb87756cff9280b81d59d9a30c795e1030dba770
serjzimmerman <52955378+serjzimmerman@users.noreply.github.com>
2023-02-24 12:00:12 +0300

    Set up basic build system. Add simple ```vkinfo``` app for displaying platform information (#2)
    * Simple build system with example
    Simple example using <vulkan.hpp> that enumerates physical devices
    
    * Configure clang-format
    
    * Specify correct api version using VK_MAKE_VERSION
    
    * Workaround for bin packing arguments in clang-format
    
    * Print physical device queue family info
    
    * Remove space before parens in function def
    
    * Change alignment style
    
    * Add CI style check with clang-format
    
    * Clean up CMakeLists formatting
    
    * Break after return type only at the top level
    
    * Delete ```uncrustify.cfg```
    
    * Specify paths to check formatting, add exclude regex
    
    * Rename ```unified_includes``` to common. Add trailing comments

---
+++ utils/vkinfo.cc
@@ -0,0 +1,108 @@
+/* Simple program that demostrates the usage of <vulkan.hpp> C++ wrappers.
+ *
+ */
+
+#include <fmt/core.h>
+
+#include <algorithm>
+#include <cstdint>
+#include <numeric>
+#include <sstream>
+#include <string>
+
+#include "common/vulkan_include.h"
+
+namespace
+{
+
+std::string
+versionToString( uint32_t version )
+{
+    const auto ver_major = VK_VERSION_MAJOR( version );
+    const auto ver_minor = VK_VERSION_MINOR( version );
+    const auto ver_patch = VK_VERSION_PATCH( version );
+
+    return fmt::format(
+        "{}.{}.{}",
+        ver_major,
+        ver_minor,
+        ver_patch // This is a workaround for clang-format
+    );
+} // versionToString
+
+void
+printPhysicalDeviceProperties( vk::PhysicalDevice device )
+{
+    const auto extensions = device.enumerateDeviceExtensionProperties();
+    const auto properties = device.getProperties();
+
+    fmt::println(
+        "Found physical device: [id = {}, type = {}, name = {}, version = {}]",
+        properties.deviceID,
+        vk::to_string( properties.deviceType ),
+        properties.deviceName,
+        versionToString( properties.apiVersion ) //
+    );
+
+    const auto extensions_string = std::accumulate(
+        extensions.begin(),
+        extensions.end(),
+        std::string{},
+        []( auto str, auto ext ) {
+            return str + " " + ext.extensionName.data();
+        } //
+    );
+
+    fmt::println(
+        "Physical device [{}] supports following extensions: {}",
+        properties.deviceName,
+        extensions_string //
+    );
+
+    fmt::println( "Physical device [{}] has following queue families", properties.deviceName );
+    const auto qf_info = device.getQueueFamilyProperties();
+    for ( uint32_t i = 0; const auto& info : qf_info )
+    {
+        fmt::println(
+            "[{}]. [queue_count = {}] of the type {}",
+            i,
+            info.queueCount,
+            vk::to_string( info.queueFlags ) //
+        );
+
+        ++i;
+    }
+} // printPhysicalDeviceProperties
+
+constexpr auto k_application_info = vk::ApplicationInfo{
+    .pApplicationName = "vkinfo",
+    .applicationVersion = VK_MAKE_VERSION( 1, 0, 0 ),
+    .pEngineName = "no engine",
+    .engineVersion = VK_MAKE_VERSION( 1, 0, 0 ),
+    .apiVersion = VK_MAKE_VERSION( 1, 1, 0 ) //
+};
+
+} // namespace
+
+int
+main()
+try
+{
+    const auto instance_create_info = vk::InstanceCreateInfo{
+        .pApplicationInfo = &k_application_info,
+    };
+
+    auto instance = vk::createInstanceUnique( instance_create_info );
+    auto physical_devices = instance->enumeratePhysicalDevices();
+
+    for ( auto device : physical_devices )
+    {
+        printPhysicalDeviceProperties( device );
+    }
+} catch ( vk::Error& e )
+{
+    fmt::println( "Vulkan error: {}", e.what() );
+} catch ( std::exception& e )
+{
+    fmt::println( "Error: {}", e.what() );
+}
