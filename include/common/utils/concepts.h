954ebd9e21a38b4c87dce313fcd0a84ef723302e
Sergei Zimmerman <serjtsimmerman@gmail.com>
2023-03-31 00:16:53 +0300

    Add file utilities

--- include/common/utils/concepts.h
+++ include/common/utils/concepts.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <concepts>
+#include <fstream>
 #include <functional>
 #include <type_traits>
 
@@ -11,6 +12,7 @@ template <typename T>
 concept Enumeration = std::is_enum_v<T>;
 
 // clang-format off
+
 template <typename T, typename D>
 concept CoercibleTo = requires ( T source ) {
     { static_cast<D>( source ) };
@@ -18,6 +20,23 @@ concept CoercibleTo = requires ( T source ) {
 
 template <typename T> concept Hashable = requires (T v) {
   { std::hash<T>{}(v) } -> std::integral;
-}; // clang-format on
+};
+
+template <typename T>
+concept IsIFStream = requires (T stream) {
+  { [] <typename CharType, typename Traits> (std::basic_ifstream<CharType, Traits> &) {} (stream) };
+};
+
+template <typename T>
+concept IsOFStream = requires (T stream) {
+  { [] <typename CharType, typename Traits> (std::basic_ofstream<CharType, Traits> &) {} (stream) };
+};
+
+template <typename T>
+concept IsFStream = requires (std::remove_cvref_t<T> stream) {
+  requires IsIFStream<T> || IsOFStream<T>;
+};
+
+// clang-format on
 
 }; // namespace utils
