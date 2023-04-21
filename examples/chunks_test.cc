04aa10d6b3b1c04db50bf30675c72320751ce1cc
krisszzzz <kris.shtrom@mail.ru>
2023-04-21 08:49:27 +0300

    [Feature]: Add std::chrono to count elapsed time

--- examples/chunks_test.cc
+++ examples/chunks_test.cc
@@ -1,26 +1,35 @@
-#include "chunk/region.h"
+#include "chunk/chunk_man.h"
+#include "chunk/chunk_mesher.h"
+#include <chrono>
 #include <iostream>
 
 int
 main()
 {
     // or decltype( auto )
-    auto& region = chunk::Region::getRef();
+    auto& region = chunk::ChunkMan::getRef();
 
-    std::cout << "Render distance: " << chunk::Region::k_render_distance << std::endl;
-    std::cout << "Chunks in Region: " << chunk::Region::k_chunks_count << std::endl;
+    std::cout << "Render distance: " << chunk::ChunkMan::k_render_distance << std::endl;
+    std::cout << "Chunks in ChunkMan: " << chunk::ChunkMan::k_chunks_count << std::endl;
 
     std::cout << "Allocated region size (in MegaBytes ): "
-              << ( chunk::Region::k_blocks_count * sizeof( chunk::BlockID ) ) / ( 1024 * 1024 ) << std::endl;
+              << ( chunk::ChunkMan::k_blocks_count * sizeof( chunk::BlockID ) ) / ( 1024 * 1024 ) << std::endl;
 
+    auto start_time = std::chrono::high_resolution_clock::now();
     // Move "forward" ( along the Y axis )
     for ( int i = 0; i <= 100; i++ )
     {
         region.changeOriginPos( { 0, i } );
     }
 
+    auto finish_time = std::chrono::high_resolution_clock::now();
+    auto elapsed_time =
+        std::chrono::duration<float, std::chrono::milliseconds::period>( finish_time - start_time ).count();
+
     auto get_chunk = region.getChunk( { -12, 100 } );
-    auto block_id = get_chunk[ { 5, 5, 3 } ];
+    auto block_id = get_chunk.at( 5, 5, 3 );
+
+    std::cout << "Elapsed time: " << elapsed_time << " millis\n";
 
     return 0;
 }
