8f6895f15845c5cb30f694472c076aa7e54c7464
krisszzzz <kris.shtrom@mail.ru>
2023-04-21 08:49:12 +0300

    [Refactor]: Rename the Region class to ChunkMan class

--- include/chunk/chunk/chunk_man.h
+++ include/chunk/chunk/chunk_man.h
@@ -32,32 +32,8 @@ namespace chunk
  * Class that contains all block ides and manage the chunks
  */
 
-class Region
+class ChunkMan
 {
-  public:
-    using ChunkMap = std::unordered_map<pos::ChunkPos, Chunk>;
-    using BlockIDPtr = std::unique_ptr<BlockID[]>;
-
-  public:
-    Region( const Region& ) = delete;
-    Region( Region&& ) = delete;
-
-    Region& operator=( const Region& ) = delete;
-    Region& operator=( Region&& ) = delete;
-    ~Region() = default;
-
-    static Region& getRef()
-    {
-        static Region singleton_region{ { 0, 0 } };
-        return singleton_region;
-    } // Region::getRef
-
-    Chunk& getChunk( const pos::ChunkPos& pos );
-    pos::ChunkPos& getOriginPos() { return m_origin_pos; }
-    const pos::ChunkPos& getOriginPos() const { return m_origin_pos; }
-
-    // Change the region origin position ( the new_origin is the chunk, where the player is )
-    void changeOriginPos( const pos::ChunkPos& new_origin );
 
   public:
     // Max chunks that can be viewed by the player
@@ -66,15 +42,40 @@ class Region
     static constexpr auto k_chunks_count = ( 2 * k_render_distance + 1 ) * ( 2 * k_render_distance + 1 );
     static constexpr auto k_blocks_count = k_chunks_count * Chunk::k_block_count;
 
+  public:
+    using ChunkMap = std::unordered_map<pos::ChunkPos, Chunk>;
+    using BlockIDPtr = std::unique_ptr<std::array<BlockID, k_blocks_count>>;
+
+  public:
+    ChunkMan( const ChunkMan& ) = delete;
+    ChunkMan( ChunkMan&& ) = delete;
+
+    ChunkMan& operator=( const ChunkMan& ) = delete;
+    ChunkMan& operator=( ChunkMan&& ) = delete;
+    ~ChunkMan() = default;
+
+    static ChunkMan& getRef()
+    {
+        static ChunkMan singleton_region{ { 0, 0 } };
+        return singleton_region;
+    } // ChunkMan::getRef
+
+    Chunk& getChunk( const pos::ChunkPos& pos );
+    pos::ChunkPos& getOriginPos() { return m_origin_pos; }
+    const pos::ChunkPos& getOriginPos() const { return m_origin_pos; }
+
+    // Change the region origin position ( the new_origin is the chunk, where the player is )
+    void changeOriginPos( const pos::ChunkPos& new_origin );
+
   private:
     /// [krisszzz] This constructor should be changed in the future
     /// because of chunk serialization ( working with file, etc.. )
-    Region( const pos::ChunkPos& origin_pos );
+    ChunkMan( const pos::ChunkPos& origin_pos );
 
   private:
     pos::ChunkPos m_origin_pos;
     ChunkMap m_chunks;
     BlockIDPtr m_block_ides;
-}; // class Region
+}; // class ChunkMan
 
 }; // namespace chunk
