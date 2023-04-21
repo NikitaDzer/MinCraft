d50f9d953e5da7183e2c4a4a5971eb35b6f39762
krisszzzz <kris.shtrom@mail.ru>
2023-04-21 08:31:42 +0300

    [Dev, Refactor]: Remove useless classes, add RenderAreaBlockPos class

--- include/chunk/chunk/position.h
+++ include/chunk/chunk/position.h
@@ -3,69 +3,119 @@
 #include <compare>
 #include <cstdint>
 
+#include "common/glm_include.h"
+
 namespace pos
 {
 
 /**
- * Class that used to find chunks in Region
+ * Class that used to find chunks in ChunkMan
  */
 
 class ChunkPos
 {
   public:
-    ChunkPos( int x_par = 0, int y_par = 0 )
+    constexpr ChunkPos( int x_par = 0, int y_par = 0 )
         : x( x_par ),
           y( y_par ){};
 
     friend bool operator==( const ChunkPos& lhs, const ChunkPos& rhs ) = default;
+
+    ChunkPos& operator+=( ChunkPos rhs )
+    {
+        x += rhs.x;
+        y += rhs.y;
+
+        return *this;
+    }
+
+    ChunkPos operator+( const ChunkPos rhs ) const
+    {
+        ChunkPos tmp = *this;
+        tmp += rhs;
+        return tmp;
+    }
+
+    ChunkPos& operator-=( ChunkPos rhs )
+    {
+        x -= rhs.x;
+        y -= rhs.y;
+
+        return *this;
+    }
+
+    ChunkPos operator-( const ChunkPos rhs ) const
+    {
+        ChunkPos tmp = *this;
+        tmp -= rhs;
+        return tmp;
+    }
 
   public:
     int x;
     int y;
 }; // class ChunkPos
 
-/**
- * Class that used to find block ides in Region
- * Note that BlockPos X and Y coordinate has nothing to do with
- * ChunkPos X and Y coordinate
+/*
+ * class that used to indexing by local coordinates in array of blocks
+ * that should be rendered
  */
 
-class BlockPos
-{
+union RenderAreaBlockPos {
   public:
-    BlockPos( int x_par = 0, int y_par = 0, uint8_t z_par = 0 )
-        : x( x_par ),
-          y( y_par ),
-          z( z_par ){};
+    constexpr RenderAreaBlockPos( uint16_t local_x, uint16_t local_y, uint16_t local_z )
+        : x( local_x ),
+          y( local_y ),
+          z( local_z ){};
+
+    RenderAreaBlockPos& operator+=( RenderAreaBlockPos rhs )
+    {
+        x += rhs.x;
+        y += rhs.y;
+        z += rhs.z;
+
+        return *this;
+    }
+
+    RenderAreaBlockPos operator+( const RenderAreaBlockPos rhs ) const
+    {
+        RenderAreaBlockPos tmp = *this;
+        tmp += rhs;
+        return tmp;
+    }
+
+    RenderAreaBlockPos& operator-=( const RenderAreaBlockPos rhs )
+    {
+        x -= rhs.x;
+        y -= rhs.y;
+        z -= rhs.z;
+
+        return *this;
+    }
+
+    RenderAreaBlockPos operator-( const RenderAreaBlockPos rhs ) const
+    {
+        RenderAreaBlockPos tmp = *this;
+        tmp -= rhs;
+        return tmp;
+    }
 
   public:
-    int x;
-    int y;
-    uint8_t z; /// The vertical coordinate Z start from 0 until 255
+    constexpr static auto max_x = 11;
+    constexpr static auto max_y = 11;
+    constexpr static auto max_z = 10;
+    // coordinate system starting right corner of
+    // render area
+    struct __attribute__( ( packed ) )
+    {
+        uint16_t x : max_x;
+        uint16_t y : max_y;
+        uint16_t z : max_z;
+    };
 
-}; // class BlockPos
+    uint32_t combined;
+};
 
-/**
- * Class describing position of block in the Chunk
- */
-
-class LocalBlockPos
-{
-  public:
-    LocalBlockPos( uint8_t x_par = 0, uint8_t y_par = 0, uint8_t z_par = 0 )
-        : x( x_par ),
-          y( y_par ),
-          z( z_par ){};
-
-    // Convert LocalBlockPos to BlockPos
-    // The position on the chunk where the block is saved required
-    // Not implemented
-    BlockPos toBlockPos( const ChunkPos& pos );
-
-  public:
-    uint8_t x;
-    uint8_t y;
-    uint8_t z;
-}; // class LocalBlockPos
+static_assert( sizeof( RenderAreaBlockPos ) == sizeof( uint32_t ) );
 
 }; // namespace pos
