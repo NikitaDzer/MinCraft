fa406311bcbcf7b02a21c422f8058b6a50b3053a
Sergei Zimmerman <serjtsimmerman@gmail.com>
2023-05-14 14:55:30 +0300

    [Fix]: Remove erroneous license header

--- src/mincraft/camera.h
+++ src/mincraft/camera.h
@@ -1,13 +1,3 @@
-/*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <tsimmerman.ss@phystech.edu>, <alex.rom23@mail.ru> wrote this file.  As long as you
- * retain this notice you can do whatever you want with this stuff. If we meet
- * some day, and you think this stuff is worth it, you can buy us a beer in
- * return.
- * ----------------------------------------------------------------------------
- */
-
 #pragma once
 
 #include "glm_include.h"
